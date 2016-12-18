7d7cb6868bdb0378dcd34f2ad74cf358dba1d4cf
gonghaima@hotmail.com <5418854188>
2016-12-19 01:45:50 +1300

    footer draft

--- src/components/Main.js
+++ src/components/Main.js
@@ -8,14 +8,14 @@ import HomeContent from '../components/home/content.jsx';
 // let yeomanImage = require('../images/yeoman.png');
 
 class AppComponent extends React.Component {
-  
+
   componentWillMount() {
-    console.log('before mt HomeContent: '+HomeContent);
+    console.log('before mt HomeContent: ' + HomeContent);
   }
   componentDidMount() {
-    console.log('after mt HomeContent: '+HomeContent);
+    console.log('after mt HomeContent: ' + HomeContent);
   }
-  
+
   render() {
     return (
       <div className="mdl-layout mdl-js-layout">
@@ -25,28 +25,28 @@ class AppComponent extends React.Component {
             <span className="mdl-layout-title">Title</span>
 
             <div className="mdl-layout-spacer"></div>
-             <div className="mdl-layout-title">Xenia Suite</div>
+            <div className="mdl-layout-title">Xenia Suite</div>
             <div className="mdl-layout-spacer"></div>
 
-            
-            
-
-<form action="#">
-  <div className="mdl-textfield mdl-js-textfield mdl-textfield--expandable">
-    <label className="mdl-button mdl-js-button mdl-button--icon" for="sample6">
-      <i className="material-icons">search</i>
-    </label>
-    <div className="mdl-textfield__expandable-holder">
-      <input className="mdl-textfield__input" type="text" id="sample6" />
-      <label className="mdl-textfield__label" for="sample-expandable">Expandable Input</label>
-    </div>
-  </div>
-</form>
 
 
 
+            <form action="#">
+              <div className="mdl-textfield mdl-js-textfield mdl-textfield--expandable">
+                <label className="mdl-button mdl-js-button mdl-button--icon" for="sample6">
+                  <i className="material-icons">search</i>
+                </label>
+                <div className="mdl-textfield__expandable-holder">
+                  <input className="mdl-textfield__input" type="text" id="sample6" />
+                  <label className="mdl-textfield__label" for="sample-expandable">Expandable Input</label>
+                </div>
+              </div>
+            </form>
 
-            
+
+
+
+
           </div></header>
         <div className="mdl-layout__drawer">
           <span className="mdl-layout-title">Title</span>
@@ -57,20 +57,16 @@ class AppComponent extends React.Component {
             <a className="mdl-navigation__link" href="">Link</a>
           </nav></div>
         <main className="mdl-layout__content  demo-layout-transparent">
-        
-        <HomeContent/>
-        
-          
+
+          <HomeContent />
+
+
         </main>
 
         <footer className="mdl-mini-footer">
-          <div className="mdl-mini-footer__left-section">
-            <div className="mdl-logo">Title</div>
-            <ul className="mdl-mini-footer__link-list">
-              <li><a href="#">Help</a></li>
-              <li><a href="#">Privacy & Terms</a></li>
-            </ul>
-          </div>
+          <div className="mdl-layout-spacer"></div>
+          <div className="trapezoid">Test</div>
+          <div className="mdl-layout-spacer"></div>
         </footer>
 
       </div>
