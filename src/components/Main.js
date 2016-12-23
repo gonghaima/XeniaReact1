547197193509a7d328ba884998191d95840fbec7
gonghaima@hotmail.com <5418854188>
2016-12-23 15:31:51 +1300

    Menu item /  Layout   - updated

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,9 +3,6 @@ require('styles/App.css');
 
 import React from 'react';
 import HomeContent from '../components/home/content.jsx';
-// var hc = require('../components/home/content');
-
-// let yeomanImage = require('../images/yeoman.png');
 
 class AppComponent extends React.Component {
 
@@ -22,7 +19,7 @@ class AppComponent extends React.Component {
         <header className="mdl-layout--fixed-header">
           <div className="mdl-layout__header-row">
 
-            <span className="mdl-layout-title">Title</span>
+            <span className="mdl-layout-title">Menu</span>
 
             <div className="mdl-layout-spacer"></div>
             <div className="mdl-layout-title">Xenia Suite</div>
@@ -33,12 +30,12 @@ class AppComponent extends React.Component {
 
             <form action="#">
               <div className="mdl-textfield mdl-js-textfield mdl-textfield--expandable">
-                <label className="mdl-button mdl-js-button mdl-button--icon" for="sample6">
+                <label className="mdl-button mdl-js-button mdl-button--icon" htmlFor="sample6">
                   <i className="material-icons">search</i>
                 </label>
                 <div className="mdl-textfield__expandable-holder">
                   <input className="mdl-textfield__input" type="text" id="sample6" />
-                  <label className="mdl-textfield__label" for="sample-expandable">Expandable Input</label>
+                  <label className="mdl-textfield__label" htmlFor="sample-expandable">Expandable Input</label>
                 </div>
               </div>
             </form>
@@ -47,14 +44,33 @@ class AppComponent extends React.Component {
 
 
 
+
+
           </div></header>
         <div className="mdl-layout__drawer">
-          <span className="mdl-layout-title">Title</span>
+
+          <div className="inline-block">
+
+            <button className="menu-button android-fab mdl-button mdl-button--colored mdl-js-button mdl-button--fab mdl-js-ripple-effect" data-upgraded=",MaterialButton,MaterialRipple">
+              <i className="material-icons">clear</i>
+              <span className="mdl-button__ripple-container"><span className="mdl-ripple is-animating"></span></span>
+            </button>
+            <span className="mdl-cell">
+              Menu
+</span>
+
+          </div>
+
+
+
           <nav className="mdl-navigation">
-            <a className="mdl-navigation__link" href="">Link</a>
-            <a className="mdl-navigation__link" href="">Link</a>
-            <a className="mdl-navigation__link" href="">Link</a>
-            <a className="mdl-navigation__link" href="">Link</a>
+            <a className="mdl-navigation__link" href="">Bookings</a>
+            <a className="mdl-navigation__link" href="">Groups</a>
+            <a className="mdl-navigation__link" href="">Functions</a>
+            <a className="mdl-navigation__link" href="">Interfaces</a>
+            <a className="mdl-navigation__link" href="">Reports</a>
+            <a className="mdl-navigation__link" href="">Company</a>
+            <a className="mdl-navigation__link" href="">Contact</a>
           </nav></div>
         <main className="mdl-layout__content  demo-layout-transparent">
 
@@ -65,7 +81,11 @@ class AppComponent extends React.Component {
 
         <footer className="mdl-mini-footer">
           <div className="mdl-layout-spacer"></div>
-          <div className="trapezoid">Test</div>
+          <div className="trapezoid">
+            <div className="innertrapezoidinner">
+              The Software, for all Medium to Large Motels, Hotels and Hotel Groups.
+          </div>
+          </div>
           <div className="mdl-layout-spacer"></div>
         </footer>
 
