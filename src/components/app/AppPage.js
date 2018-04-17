a91c266699873bd9d931986d926bda7a10e057e1
steven <gonghaima@hotmail.com>
2018-04-17 22:36:33 +1000

    Add descriptio section - Mobile app

--- src/components/app/AppPage.js
+++ src/components/app/AppPage.js
@@ -21,27 +21,75 @@ class AppPage extends Component {
         <Slider {...settings}>
           <div>
             <img src="../images/app/2018-04-12_04-02-30-PM_iphone6_gold_portrait.png" />
+            <div className="descriptionContainer">
+              <ul>
+                <li>Key features: Even if your app has 20 different functions, focus on mentioning only the core features. You can highlight each of these features by showing how it will help your customers. Make use of bullet points here, as it is friendly to fast reading.</li>
+                <li>Credibility: The description should also include a credibility statement. The best way to do this for a local business is by including a one-sentence customer review alluding to the app’s value. This third-party endorsement will help to build credibility when there are no reviews for your app yet.</li>
+              </ul>
+            </div>
           </div>
           <div>
             <img src="../images/app/2018-04-12_04-02-36-PM_iphone6_gold_portrait.png" />
+            <div className="descriptionContainer">
+              <ul>
+                <li>Key features: Even if your app has 20 different functions, focus on mentioning only the core features. You can highlight each of these features by showing how it will help your customers. Make use of bullet points here, as it is friendly to fast reading.</li>
+                <li>Credibility: The description should also include a credibility statement. The best way to do this for a local business is by including a one-sentence customer review alluding to the app’s value. This third-party endorsement will help to build credibility when there are no reviews for your app yet.</li>
+              </ul>
+            </div>
           </div>
           <div>
             <img src="../images/app/2018-04-12_04-02-50-PM_iphone6_gold_portrait.png" />
+            <div className="descriptionContainer">
+              <ul>
+                <li>Key features: Even if your app has 20 different functions, focus on mentioning only the core features. You can highlight each of these features by showing how it will help your customers. Make use of bullet points here, as it is friendly to fast reading.</li>
+                <li>Credibility: The description should also include a credibility statement. The best way to do this for a local business is by including a one-sentence customer review alluding to the app’s value. This third-party endorsement will help to build credibility when there are no reviews for your app yet.</li>
+              </ul>
+            </div>
           </div>
           <div>
             <img src="../images/app/2018-04-12_04-03-45-PM_iphone6_gold_portrait.png" />
+            <div className="descriptionContainer">
+              <ul>
+                <li>Key features: Even if your app has 20 different functions, focus on mentioning only the core features. You can highlight each of these features by showing how it will help your customers. Make use of bullet points here, as it is friendly to fast reading.</li>
+                <li>Credibility: The description should also include a credibility statement. The best way to do this for a local business is by including a one-sentence customer review alluding to the app’s value. This third-party endorsement will help to build credibility when there are no reviews for your app yet.</li>
+              </ul>
+            </div>
           </div>
           <div>
             <img src="../images/app/2018-04-12_04-04-23-PM_iphone6_gold_portrait.png" />
+            <div className="descriptionContainer">
+              <ul>
+                <li>Key features: Even if your app has 20 different functions, focus on mentioning only the core features. You can highlight each of these features by showing how it will help your customers. Make use of bullet points here, as it is friendly to fast reading.</li>
+                <li>Credibility: The description should also include a credibility statement. The best way to do this for a local business is by including a one-sentence customer review alluding to the app’s value. This third-party endorsement will help to build credibility when there are no reviews for your app yet.</li>
+              </ul>
+            </div>
           </div>
           <div>
             <img src="../images/app/2018-04-12_04-04-49-PM_iphone6_gold_portrait.png" />
+            <div className="descriptionContainer">
+              <ul>
+                <li>Key features: Even if your app has 20 different functions, focus on mentioning only the core features. You can highlight each of these features by showing how it will help your customers. Make use of bullet points here, as it is friendly to fast reading.</li>
+                <li>Credibility: The description should also include a credibility statement. The best way to do this for a local business is by including a one-sentence customer review alluding to the app’s value. This third-party endorsement will help to build credibility when there are no reviews for your app yet.</li>
+              </ul>
+            </div>
           </div>
           <div>
             <img src="../images/app/2018-04-12_04-06-09-PM_iphone6_gold_portrait.png" />
+            <div className="descriptionContainer">
+              <ul>
+                <li>Key features: Even if your app has 20 different functions, focus on mentioning only the core features. You can highlight each of these features by showing how it will help your customers. Make use of bullet points here, as it is friendly to fast reading.</li>
+                <li>Credibility: The description should also include a credibility statement. The best way to do this for a local business is by including a one-sentence customer review alluding to the app’s value. This third-party endorsement will help to build credibility when there are no reviews for your app yet.</li>
+              </ul>
+            </div>
           </div>
           <div>
             <img src="../images/app/2018-04-12_04-06-24-PM_iphone6_gold_portrait.png" />
+            <div className="descriptionContainer">
+              <ul>
+                <li>Key features: Even if your app has 20 different functions, focus on mentioning only the core features. You can highlight each of these features by showing how it will help your customers. Make use of bullet points here, as it is friendly to fast reading.</li>
+                <li>Credibility: The description should also include a credibility statement. The best way to do this for a local business is by including a one-sentence customer review alluding to the app’s value. This third-party endorsement will help to build credibility when there are no reviews for your app yet.</li>
+              </ul>
+            </div>
           </div>
         </Slider>
       </div>
